672ef0600f92095d88ba75eb7286ac3251f1158a
dtypo <40309637+dtypo@users.noreply.github.com>
2022-07-12 17:31:57 +0200

    Update auto_compose.sh

--- auto_compose.sh
+++ auto_compose.sh
@@ -3,7 +3,7 @@
 TEST_DIR="/volume1/homes"
 DOCKER_DIR="/volume1/docker"
 
-if [ -d "$DIR" ]; then
+if [ -d "$TEST_DIR" ]; then
 	YML_LIST=$(cat /volume1/docker/docker-compose.yml|grep "container_name"|awk '$2 != "container_name:" {print $2}'|sort);
 	RUNNING_LIST=$(docker ps | awk '$NF != "NAMES" {print $NF}'|sort);
 	if [ "$YML_LIST" != "$RUNNING_LIST" ]; then
