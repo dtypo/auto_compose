36bc366e2b5282b78a1d83549ea85634840e72c3
dtypo <40309637+dtypo@users.noreply.github.com>
2022-07-12 17:19:06 +0200

    Create auto_compose.sh

---
+++ auto_compose.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TEST_DIR="/volume1/homes"
+DOCKER_DIR="/volume1/docker"
+
+if [ -d "$DIR" ]; then
+	YML_LIST=$(cat /volume1/docker/docker-compose.yml|grep "container_name"|awk '$2 != "container_name:" {print $2}'|sort);
+	RUNNING_LIST=$(docker ps | awk '$NF != "NAMES" {print $NF}'|sort);
+	if [ "$YML_LIST" != "$RUNNING_LIST" ]; then
+		docker-compose -f $DOCKER_DIR/docker-compose.yml up -d --remove-orphans
+	fi
+fi
